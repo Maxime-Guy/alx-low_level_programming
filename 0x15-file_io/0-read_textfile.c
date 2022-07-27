b496bce82db4e2b77f62107bedaccefc11d09096
Maxime-Guy <guymaximebakunzi@gmail.com>
2022-07-27 04:53:45 -0700

    C program about file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,46 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: the name of the file to be written
+ * @letters: the number of letters to read and print
+ * Return: the number of letters it read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
+	int fd, numwrit, numread;
+	char *buf = malloc(sizeof(char) * (letters + 1));
+
+	if (buf == NULL)
 		return (0);
+	if (filename == NULL || letters == 0)
+	{
+		free(buf);
+		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
-	buf[i] = '\0';
+	numread = read(fd, buf, letters);
+	if (numread < 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	buf[letters] = '\0';
+	numwrit = write(STDOUT_FILENO, buf, numread);
+	if (numwrit <= 0)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
 	free(buf);
-	return (y);
+	return (numwrit);
 }
